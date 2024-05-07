86ecbb1e2590931ccdfefc46c81c4ac1328caaad
Thiago Frank <thiagofrankas@gmail.com>
2024-05-07 20:56:17 -0300

    Comeco

---
+++ src/functions/calcularMedia.ts
@@ -0,0 +1,12 @@
+function calcularMedia(nota1:number, nota2:number, nota3:number){
+    const peso1 = 4;
+    const peso2 = 5;
+    const peso3 = 6;
+
+    let media;
+
+    return media = ((nota1*peso1) + (nota2*peso2) + (nota3*peso3))/15;
+
+}
+
+export {calcularMedia};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        pretro: "#000",
+        white: "#fff"
+      }
     },
   },
   plugins: [],
